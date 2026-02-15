37a24c08e947d0d766c0b449b57e56bbfaa3b3fc
Varshit Kumar <153614486+tyagivarshit@users.noreply.github.com>
2026-02-15 16:52:39 +0530

    Update PollPage.jsx

--- client/src/PollPage.jsx
+++ client/src/PollPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom"
 import axios from "axios"
 import { io } from "socket.io-client"
 
-const API_URL = "https://web-poll-new.onrender.com"
+const API_URL = import.meta.env.VITE_API_URL
 const socket = io(API_URL)
 
 function PollPage() {
